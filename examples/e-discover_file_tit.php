a8bda4e737a4471ff5cd1832dc2f9ead3a86951b
Carsten Klee <carsten.klee@sbb.spk-berlin.de>
2013-10-23 10:36:12 +0200

    better output handling for command line

--- examples/e-discover_file_tit.php
+++ examples/e-discover_file_tit.php
@@ -6,7 +6,7 @@
 * file that was distributed with this source code.
 */
 use CK\MARC2RDF as m2r;
-include('../../marc2rdf/autoload.php');
+include('../autoload.php');
 foreach (glob('my_callback/callback_*.php') as $filename) include $filename;
 $test = new m2r\MARCFILE2RDF("../template/default.jsonld","marc/e-discover.mrc");
 print '<html><head><meta charset="UTF-8" /></head><body><pre>'.htmlspecialchars($test->output('turtle')).'</pre></body></html>';

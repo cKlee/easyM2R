fdc0ff31bfb1e94b7d9249c07c40bddac2e680ea
Carsten Klee <carsten.klee@sbb.spk-berlin.de>
2013-10-24 18:44:43 +0200

    no custom callbacks for this example

--- examples/e-discover_file_tit.php
+++ examples/e-discover_file_tit.php
@@ -7,6 +7,5 @@
 */
 use CK\MARC2RDF as m2r;
 include('../autoload.php');
-foreach (glob('my_callback/callback_*.php') as $filename) include $filename;
 $test = new m2r\MARCFILE2RDF("../template/default.jsonld","marc/e-discover.mrc");
 print '<html><head><meta charset="UTF-8" /></head><body><pre>'.htmlspecialchars($test->output('turtle')).'</pre></body></html>';
